9d702601c2cf1c211d97d2dabee502b52c226034
Timofey Garnyk <tigar@servers.com>
2018-04-18 13:57:36 +0300

    Fail in task_create if reply->error not empty

--- libraries/kapacitor_api.rb
+++ libraries/kapacitor_api.rb
@@ -8,7 +8,7 @@ module KapacitorCookbook
       options[:method] = 'Post'
       options[:endpoint] = '/kapacitor/v1/tasks'
       task = _do_request(options, task.to_json)
-      raise StandardError, "#{task['error']}" if task['error'] =~ /invalid TICKscript:/
+      raise StandardError, "#{task['error']}" unless task['error'].empty?
     rescue BackendError
       nil
     end
